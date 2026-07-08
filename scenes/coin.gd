d74579c7ec840e6a38567184df8a106518e76297
crystal <cheesedrone@gmail.com>
2026-07-08 14:59:49 -0700

    coin resources

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -2,11 +2,12 @@ extends Area2D
 @onready var coins = Global.coins
 @onready var anim = $AnimatedSprite2D
 @export var type : CoinType
-var animated : bool = false
+var unlock_animation : bool = false
 
 func _ready() -> void:
 	anim.sprite_frames = type.anim
-	anim.play()
+	if unlock_animation:
+		anim.play()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
