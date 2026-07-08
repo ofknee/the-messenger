86fef45e604b2fb1ffecb6fb4fa770d0205b74ae
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-08 14:39:08 -0700

    updated unlock_animation boolean - for now, use q ("dead") to trigger resume of animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,30 +5,42 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
 @onready var animated_sprite = $AnimatedSprite2D
+var unlock_animation = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-			
+	if Input.is_action_just_pressed("dead"):
+		if unlock_animation == false:
+			unlock_animation = true
+		else:
+			unlock_animation = false
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animated_sprite.play("fall")
+		if unlock_animation == true:
+			animated_sprite.play("fall")
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animated_sprite.play("jump")
+		if unlock_animation == true:
+			animated_sprite.play("jump")
 		
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("walk_l", "walk_r")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite.play("run")
-		if direction < 0:
-			animated_sprite.flip_h = false
-		if direction > 0:
-			animated_sprite.flip_h = true
+		if unlock_animation == true:
+			animated_sprite.play("run")
+			if direction < 0:
+				animated_sprite.flip_h = false
+			if direction > 0:
+				animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite.play("idle")
+		if unlock_animation == true:
+			animated_sprite.play("idle")
+		#if unlock_animation == false:
+			#animated_sprite.stop()
 
 	move_and_slide()
