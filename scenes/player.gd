692ab1c5015bcdc360e0d4e31dd0ca59c0b7581d
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-08 12:25:42 -0700

    velocity

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,7 +9,7 @@ const JUMP_VELOCITY = -450.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.3
 		animated_sprite.play("fall")
 		
 	# Handle jump.
