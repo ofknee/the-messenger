d38ce3cab6177ba90b0dd9957575c616c0c12eb8
crystal <cheesedrone@gmail.com>
2026-07-09 15:02:40 -0700

    assets and currency updates

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,10 +1,11 @@
 extends CanvasLayer
 
-@onready var label = $Control/MarginContainer/Panel/MarginContainer/HBoxContainer/RichTextLabel
+@onready var label = $Control/MarginContainer/Panel/HBoxContainer/RichTextLabel
 
 func _ready() -> void:
 	Global.currency_changed.connect(update_coins)
 	update_coins(Global.coins)
 
-func update_currency(coins: int) -> void:
+
+func update_coins(coins: int) -> void:
 	label.text = str(coins)
