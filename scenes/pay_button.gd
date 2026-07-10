b65e8142f6221bcf8d78f51a116fe99bd353ed67
crystal <cheesedrone@gmail.com>
2026-07-10 10:20:08 -0700

    pay button logic

--- scenes/pay_button.gd
+++ scenes/pay_button.gd
@@ -1,19 +1,32 @@
 extends Button
 @export var price : int = 1
-#@export var thing : String = "EXAMPLE"
+@export var thing : String = "example" # thing to buy
 @onready var price_display = $Price
 @onready var label = $Label
+var bought : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setup()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func setup():
 	label.text = name
 	price_display.text = str(price)
-	
+
+func _on_pressed() -> void:  ##spawn eye laccording to button type
+	if payable():
+		if bought == false:
+			Global.coins -= price
+			print("bought: ",thing)
+			modulate.a = 0.5
+			bought = true
+		else:
+			print("alr bought: ",thing)
+	else:
+		print("ur broke lol")
+
+func payable() -> bool:
+	if price <= Global.coins:
+		return true
+	else:
+		return false
