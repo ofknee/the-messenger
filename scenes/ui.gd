faeeffec570e6d109064d51140068ec3c8aa95d3
crystal <cheesedrone@gmail.com>
2026-07-09 18:05:06 -0700

    shop button

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,12 +1,13 @@
 extends CanvasLayer
 
 @onready var label = $Control/MarginContainer/Bank/HBoxContainer/MarginContainer2/RichTextLabel
-#@onready var panel = $Control/MarginContainer/Bank
-#@onready var hbox = $Control/MarginContainer/Bank/HBoxContainer
+
 func _ready() -> void:
 	Global.currency_changed.connect(update_coins)
 	update_coins(Global.coins)
 
-
 func update_coins(coins: int) -> void:
 	label.text = str(coins)
+	
+func _on_button_pressed() -> void:
+	print("open shop")
