e80ed1307b41afaf8791432a3bc8ea140a320247
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-10 10:05:12 -0700

    beginnings of coin sound dev

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
 @onready var anim = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 @export var type : CoinType
 var unlock_animation : bool = true
+var unlock_sound : bool = false
 
 func _ready() -> void:
 	anim.sprite_frames = type.anim
@@ -11,5 +13,10 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		Global.coins += type.value
-	queue_free()
+	#if Input.is_action_just_pressed("terror_mode"):
+		#unlock_sound = true
+	#if unlock_sound == true:
+		animation_player.play("pickup")
+	#else:
+		#queue_free()
 	print(Global.coins)
