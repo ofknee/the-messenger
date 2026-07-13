dc4ed85cae1231c06c5096a3069151c51c6bc171
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-13 11:15:46 -0700

    teleport animation

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -5,6 +5,7 @@ extends Area2D
 	#if this collision coord lower, tp to higher checkpoint
 
 @onready var player = $"../../Player"
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var check_node = %checkpoints
 #on checkpoint exited, set highest y to checkpoint pos
 var change_save
@@ -26,12 +27,16 @@ func _is_higher(node: Area2D):
 		if node.position.y < Global.high_check:
 			Global.high_check = node.position.y
 			Global.high_checkpoint = node
-			print('high_check: ') 
-			print(Global.high_check)
-			print('high_checkpoint: ') 
-			print(Global.high_checkpoint)
+			if unlocked["animation"] == true:
+				animated_sprite.play("open")
+			#print('high_check: ') 
+			#print(Global.high_check)
+			#print('high_checkpoint: ') 
+			#print(Global.high_checkpoint)
 		elif node.position.y > Global.high_check:
 			#playposition = Global.high_checkpoint.position
+			
+				#await get_tree().create_timer(0.8).timeout
 			player.global_position.x = Global.high_checkpoint.global_position.x
 			player.global_position.y = Global.high_checkpoint.global_position.y + 20
 
