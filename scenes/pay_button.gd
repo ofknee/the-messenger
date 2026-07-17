923abb9555103b319d24d0b72f8bdc1c36983f04
crystal <cheesedrone@gmail.com>
2026-07-17 10:13:48 -0700

    tooltip code

--- scenes/pay_button.gd
+++ scenes/pay_button.gd
@@ -3,11 +3,15 @@ extends Button
 @export var thing : String = "example" # thing to buy
 @onready var price_display = $Price
 @onready var label = $Label
+@onready var tooltip = $Tooltip
 var bought : bool = false
 var l1unlocks = Global.unlocked.keys()
 var l2unlocks = ["checkpoints", "terrorMode"]
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
+	mouse_entered.connect(on_mouse_entered)
+	mouse_exited.connect(one_mouse_exited)
 	disabled = true
 	setup()
 	Global.currency_changed.connect(payable)
